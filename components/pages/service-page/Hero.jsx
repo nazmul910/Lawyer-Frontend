c425b276a0917dbe9705c0da7f07bddb2adf6d2a
nazmul910 <islamnasir910@gmail.com>
2026-06-16 20:34:20 +0600

    feat: enhance Hero component with GSAP animations and update styling

--- components/pages/service-page/Hero.jsx
+++ components/pages/service-page/Hero.jsx
@@ -1,17 +1,48 @@
+"use client"
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+
+
+
 export default function Hero() {
+
+  const text1 = useRef(null);
+  const text2 = useRef(null);
+  const text3 = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(() =>{
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: text1.current,
+        start: "top 80%",
+        end: "bottom 20%",
+        toggleActions: "play none none reverse",
+      },
+    });
+
+    tl.from(text1.current, { opacity: 0, y: 50, duration: 0.5 })
+      .from(text2.current, { opacity: 0, y: 50, duration: 0.5 }, "-=0.3")
+      .from(text3.current, { opacity: 0, y: 50, duration: 0.5 }, "-=0.3");
+  })
+
   return (
     <>
       <section className="bg-[linear-gradient(135deg,#0e0e0e,#222C30,#0e0e0e)]">
         <div className="container1 h-[70vh]">
-          <div className="flex flex-col items-center justify-center h-full text-center text-white">
+          <div className="flex flex-col md:items-center justify-center h-full md:text-center text-white">
             <div>
-              <p className="text-[18px] uppercase font-pt_serif">Practice Areas</p>
+              <p ref={text1} className="text-[18px] mb-5 md:mb-0 lg:mb-2 2xl:mb-0 uppercase font-pt_serif">Practice Areas</p>
             </div>
-            <div className="w-[70%]">
-              <h1 className="text-[86px] text-center font-playfair letter-spacing-[-3px] leading-[86px] font-medium uppercase">Comprehensive legal support</h1>
+            <div className=" md:w-[70%] 2xl:w-[65%]">
+              <h1 ref={text2} className="text-[36px]  leading-[40px] md:text-[40px] lg:text-[60px] 2xl:text-[86px] md:text-center font-playfair lg:tracking-[-3px] 2xl:tracking-[-4px] md:leading-[45px]  lg:leading-[60px] 2xl:leading-[86px] font-medium uppercase">Comprehensive legal support</h1>
             </div>
-            <div className="w-1/3 mt-4">
-              <p className="text-[20px]">
+            <div className="md:w-[70%] lg:w-1/2 2xl:w-1/3 mt-5 md:mt-2 lg:mt-4">
+              <p ref={text3} className="text-[16px] md:text-[20px]">
                 We provide expert advice and advocacy across a wide range of
                 legal issues.
               </p>
