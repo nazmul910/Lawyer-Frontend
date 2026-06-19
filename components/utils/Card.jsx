112e9de8bc8e2c46f0a0f7d9b20e8b3f57c3b35f
nazmul910 <islamnasir910@gmail.com>
2026-06-19 22:27:43 +0600

    fix: add red text color for "No Data Here" message in Card component

--- components/utils/Card.jsx
+++ components/utils/Card.jsx
@@ -15,7 +15,7 @@ export default function Card() {
 //   }
 
   if (!lawyerData || lawyerData.length === 0) {
-    return <p className="text-center py-10">No Data Here</p>;
+    return <p className="text-center py-10 text-red-500">No Data Here</p>;
   }
 
   return (
